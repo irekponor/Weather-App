9f46dad470d10b65eb3e0138a1711b8efa60c27b
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 04:37:47 +0100

    API

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function displayWeather(data) {
   const weatherIcon = document.getElementById("weather-icon");
   const hourlyForecastDiv = document.getElementById("hourly-forecast");
 
-  // Clear previous content
+  // removoing previous content
   weatherInfoDiv.innerHTML = "";
   hourlyForecastDiv.innerHTML = "";
   tempDivInfo.innerHTML = "";
@@ -46,7 +46,7 @@ function displayWeather(data) {
     weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
   } else {
     const cityName = data.name;
-    const temperature = Math.round(data.main.temp - 273.15); // Convert to Celsius
+    const temperature = Math.round(data.main.temp - 273.15);
     const description = data.weather[0].description;
     const iconCode = data.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
