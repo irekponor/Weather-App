1e0720b3ff97dd797a0f6625bea97bbc2187d797
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 04:15:53 +0100

    weather

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function getWeather() {
   const apiKey = "5b9bb410feffebe339dce2482a4509ff";
   const city = document.getElementbyId("city").value;
+
+  if (!city) {
+    alert("please, enter a city");
+    return;
+  }
+
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+  fetch(currentWeatherUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeather(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching current weather data:", error);
+      alert("Error fetching current weather data. Try again.");
+    });
 }
