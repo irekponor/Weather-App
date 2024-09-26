660a4cf6440441c097168fd0bdfb9f4d05c9f176
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 04:23:47 +0100

    weather

--- script.js
+++ script.js
@@ -19,4 +19,26 @@ function getWeather() {
       console.error("Error fetching current weather data:", error);
       alert("Error fetching current weather data. Try again.");
     });
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      displayHourlyForecast(data.list);
+    })
+    .catch((error) => {
+      console.error("Error fetching hourly forecast data:", error);
+      alert("Error fetching hourly forecast data. Try again.");
+    });
+}
+
+function displayWeather(data) {
+  const tempDivInfo = document.getElementById("temp-div");
+  const weatherInfoDiv = document.getElementById("weather-info");
+  const weatherIcon = document.getElementById("weather-icon");
+  const hourlyForecastDiv = document.getElementById("hourly-forecast");
+
+  // removing previous content
+  weatherInfoDiv.innerHTML = "";
+  hourlyForecastDiv.innerHTML = "";
+  tempDivInfo.innerHTML = "";
 }
