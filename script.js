8bafeb01329c7a0ee907bf22b423ce058eb0d17b
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 04:29:54 +0100

    weather

--- script.js
+++ script.js
@@ -71,4 +71,24 @@ function displayWeather(data) {
 
 function displayHourlyForecast(hourlyData) {
   const hourlyForecastDiv = document.getElementById("hourly-forecast");
+
+  const next24Hours = hourlyData.slice(0, 8);
+
+  next24Hours.forEach((item) => {
+    const dateTime = new Date(item.dt * 1000);
+    const hour = dateTime.getHours();
+    const temperature = Math.round(item.main.temp - 273.15);
+    const iconCode = item.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+
+    const hourlyItemHtml = `
+            <div class="hourly-item">
+                <span>${hour}:00</span>
+                <img src="${iconUrl}" alt="Hourly Weather Icon">
+                <span>${temperature}°C</span>
+            </div>
+        `;
+
+    hourlyForecastDiv.innerHTML += hourlyItemHtml;
+  });
 }
